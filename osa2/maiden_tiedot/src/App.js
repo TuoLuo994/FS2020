22e4c1d7ead75ad3251350f8c6f656890418875b
Tuomas Luojus <tuomas.luojus@tuni.fi>
2020-05-27 15:09:06 +0300

    2.14

--- osa2/maiden_tiedot/src/App.js
+++ osa2/maiden_tiedot/src/App.js
@@ -1,45 +1,34 @@
 import React, { useState, useEffect } from 'react'
 import axios from 'axios'
 
-
-const CountryInfo = ({country}) => {
-  if(country.length===1){
-    var cou = country[0]
-    return(
-      <div>
-        <h1>{cou.name}</h1>
-        <div>capital {cou.capital}</div>
-        <div>population {cou.population}</div>
-        <h2>languages</h2>
-        <ul>
-        {cou.languages.map((lang) =>
-          <li key={lang.name}>{lang.name}</li>
-        )}
-        </ul>
-        <img width='300px' alt='flag' src={cou.flag} />
-      </div>
-
-    )
-  }
-  return(<div></div>)
-}
-
 const App = (props) => {
   const [ filter, setFilter ] = useState('')
   const [ countries, setCountries ] = useState([])
   const [ filtered, setFiltered ] = useState([])
+  const [ weather, setWeather ] = useState([])
+  const [ query, setQuery ] = useState('')
 
   useEffect(() => {
     console.log('effect')
     axios
       .get('https://restcountries.eu/rest/v2/all')
       .then(response => {
-        console.log('promise fulfilled')
+        console.log('country promise fulfilled')
         setCountries(response.data)
         setFiltered(response.data)
       })
   }, [])
   console.log('render', countries.length, 'countries')
+  const access_key = process.env.REACT_APP_API_KEY
+  useEffect(() => {
+    console.log('effect')
+    axios
+      .get(`http://api.weatherstack.com/current?access_key=${access_key}&query=${query}`)
+      .then(response => {
+        console.log('weather promise fulfilled')
+        setWeather(response.data)
+      })
+  }, [access_key, query])
 
   const handlefilterChange = (event) => {
     var countryList = []
@@ -48,12 +37,16 @@ const App = (props) => {
         countryList.push(country)
       }
     })
+    if (countryList.length > 0){
+      setQuery(countryList[0].name)
+    }
     setFiltered(countryList)
     setFilter(event.target.value)
   }
 
   const handleClick = (country) => {
     setFiltered([country])
+    setQuery(country.name)
   }
 
   const listCountries = (c) => {
@@ -67,7 +60,6 @@ const App = (props) => {
         <button onClick={() => handleClick(country)}>text</button>
       </div>
     ))}
-  console.log(filtered)  
   return (
     <div>
       find countries <input 
@@ -75,12 +67,33 @@ const App = (props) => {
                       onChange={handlefilterChange}
                      />
       <div>
-        {filtered.length > 10 
+        {filtered.length > 10
         ? <div>Too many matches, specify another filter</div>
         : listCountries(filtered)
       }
       </div>
-      <CountryInfo country={filtered} />
+        <div>
+          {filtered.length === 1 && filtered.length > 0
+          ? 
+          <div>
+            <h1>{filtered[0].name}</h1>
+            <div>capital {filtered[0].capital}</div>
+            <div>population {filtered[0].population}</div>
+            <h2>Languages</h2>
+            <ul>
+            {filtered[0].languages.map((lang) =>
+              <li key={lang.name}>{lang.name}</li>
+            )}
+            </ul>
+            <img width='300px' alt='flag' src={filtered[0].flag} />
+            <h2>Weather in {weather.location.name}</h2>
+            <div>
+              <b>temperature: {weather.current.temperature}</b>
+            </div>
+          </div>
+          : <div></div>
+        }
+        </div>
     </div>
   );
 }
